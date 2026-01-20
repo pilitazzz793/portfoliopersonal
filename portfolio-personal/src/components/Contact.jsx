f0fd770977cd16731f098f5f2010868ebc3d6e19
Pilar <nadaalpilarr@gmail.com>
2026-01-19 21:29:32 -0300

    apartado de contactos actualizado

--- portfolio-personal/src/components/Contact.jsx
+++ portfolio-personal/src/components/Contact.jsx
@@ -1,21 +1,44 @@
 import {motion} from 'framer-motion';
 
+
 function Contact (){
-    return(
+    return (
         <motion.section
-        initial={{opacity:0}}
-        whileInView={{opacity:1}}
-        transition={{duration: 0.6}}
+        id="contact"
+        initial={{ opacity:0, y: 30}}
+        whileInView={{ opacity: 1, y: 0}}
+        transition={{ duration: 0.6, ease: "easeOut"}}
         viewport={{once: true}}
+        className="contact-section"
         >
 
             <h2>Contactame!</h2>
-            <p>¿Tienes alguna pregunta o proyecto en mente? ¡No dudes en contactarme!</p>
-            <p>Email: nadaalpilarr@gmail.com</p>
-            <p>GitHub: github.com/pilitazzz793</p>
+            <p className='contact-text
+            '>
+                ¿Tenes una idea, proyecto o simplemente queres charlar?
+                Estoy abierta a nuevas oportunidades y colaboraciones.
+
+            </p>
+            <ul className="contact-list">
+        <li>
+          {" "}
+          <a href="mailto:nadaalpilarr@gmail.com">
+            nadaalpilarr@gmail.com
+          </a>
+        </li>
+
+        <li>
+          {" "}
+          <a
+            href="https://github.com/pilitazzz793"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.com/pilitazzz793
+          </a>
+        </li>
+      </ul>
         </motion.section>
     );
-
 }
-
 export default Contact;
